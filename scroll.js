71580838d4fd7a73640d1198e2148458312f573d
jmsd <sanchezdiezma@gmail.com>
2022-06-14 22:32:13 +0200

    version 1 - preversion a falta de implementar scroll dinamico y refactorizar codigo

---
+++ scroll.js
@@ -0,0 +1,41 @@
+
+document.lastScrollPosition = 0;
+document.lastCentered = 0;
+document.onWayTo = null
+
+document.addEventListener('scroll',() => {
+    const direction = window.scrollY - document.lastScrollPosition > 0 ? 'down' : 'up';
+    const sections = [...document.querySelectorAll('section')];
+
+        if (document.onWayTo === null) {
+
+            const destIndex = direction === 'up' ? document.lastCentered - 1 : document.lastCentered + 1;
+            if (destIndex >= 0 && destIndex < sections.length) {
+                
+                document.onWayTo = destIndex;
+                window.scroll(0, sections[destIndex].offsetTop);
+            }
+        }
+
+        
+        
+
+
+    sections.forEach((section,index) => {
+        if(window.scrollY === section.offsetTop) {
+            document.lastCentered = index;
+            section.className = 'active';
+            if (document.onWayTo === index) {
+                document.onWayTo = null;
+            }
+        } else {
+            section.className = '';
+        }
+});
+            
+
+        
+
+    document.lastScrollPosition = window.scrollY;
+})
+
